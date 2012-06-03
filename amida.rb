31af313d04349d9a45c2c007b5d69c4f80585ade
Suguru Odai <ourqwers@gmail.com>
2012-06-04 02:02:09 +0900

    １行あたり複数の横線を引くことがあるように修正

--- amida.rb
+++ amida.rb
@@ -1,45 +1,62 @@
 class Amida
   attr_reader :player, :row, :hit
 
-  def initialize(player_count)
+  def initialize(player_count,row_count)
     @player_count = player_count
     @player = []
     @row = []
     @hit = Array.new(@player_count,"    ")
 
     setting_player
+    row_count.times {setting_row}
     setting_hit
   end
 
+  private
+
   def setting_player
-    1.upto(@player_count) do |i|
-      @player << (64 + i).chr
+    @player_count.times do |i|
+      @player << (65 + i).chr
     end
   end
 
-  def line
-    current_row = []
+  def setting_row
+    current_row = Array.new(@player_count,"|")
+    connect_pos = []
 
-    1.upto(@player_count) do |i|
-      current_row << "|"
-      current_row << "   "
+    (@player_count / 2).times do
+      next_pos = pc_rand
+      unless direct_connect?(connect_pos,next_pos)
+        connect_pos << next_pos
+      end
     end
 
-    pos = rand(@player_count) * 2 + 1
-    current_row[pos] = "---"
-    current_row.pop
-    current_row.push("\n")
+    current_row.map!.with_index do |line,pos|
+      if connect_pos.index(pos)
+        "#{line}---"
+      else
+        "#{line}   "
+      end
+    end
+
+    current_row[@player_count - 1] = "|\n"
     @row << current_row
   end
 
   def setting_hit
-    pos = rand(@player_count)
-    @hit[pos] = "!!!"
+    @hit[pc_rand] = "!!!"
+  end
+
+  def pc_rand
+    rand(@player_count)
+  end
+
+  def direct_connect?(connect_pos,next_pos)
+    connect_pos.index(next_pos - 1) or connect_pos.index(next_pos + 1)
   end
 end
 
-amida = Amida.new(ARGV[0].to_i)
-0.upto(10) {amida.line}
+amida = Amida.new(ARGV[0].to_i, 10)
 
 puts amida.player.join("   ")
 puts amida.row.join
